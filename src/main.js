004ae777fd1400bf9ead070fad7e2225716a68c4
Sahara <sahararod@gmail.com>
2023-10-10 10:12:05 -0600

    quitando archivo no usable de recoverPassword y sus referencias

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@
 import login from './components/login.js';
 import error from './components/error.js';
 import recover from './components/recover.js';
-import reset from './components/resetPassword.js';
 import newUser from './components/NewUserForm.js';
 import preferences from './components/preferences.js';
-import timeLine from './components/TimeLine.js';
+import timeLine from './components/timeLine.js';
 
 const root = document.getElementById('root');
 
@@ -13,7 +12,6 @@ const routes = [
   { path: '/', component: login },
   { path: '/error', component: error },
   { path: '/recover', component: recover },
-  { path: '/resetPassword', component: reset },
   { path: '/newUser', component: newUser },
   { path: '/preferences', component: preferences },
   { path: '/timeLine', component: timeLine },
